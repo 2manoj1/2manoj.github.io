851a12c3424ec8f86c8d45d5a90950eb142d9d66
Manoj Mukherjee <Manoj.Mukherjee@optiv.com>
2019-09-24 14:01:23 +0530

    intial commit

---
+++ precache-manifest.926fd62fa1124380b5e10f7559ceb4ad.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc7b8e7d64fe86134bafe6b58a36cb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d02a47439b3cf1d1502",
+    "url": "/static/css/main.5fbbdd89.chunk.css"
+  },
+  {
+    "revision": "1c06872badd8ea0f3314",
+    "url": "/static/js/2.96fdda44.chunk.js"
+  },
+  {
+    "revision": "1d02a47439b3cf1d1502",
+    "url": "/static/js/main.19fb8a0f.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "4fecfaadb34f86867d0c7ae5c25e315f",
+    "url": "/static/media/Background_Circle.4fecfaad.png"
+  },
+  {
+    "revision": "9b14f9e6765b6de220768a0df0fc9109",
+    "url": "/static/media/a123.9b14f9e6.png"
+  },
+  {
+    "revision": "7f5bc7f3913b72e421983e869024afed",
+    "url": "/static/media/ap.7f5bc7f3.png"
+  },
+  {
+    "revision": "155af7fd1e56340dc2cec7bd2761e203",
+    "url": "/static/media/brideback.155af7fd.jpg"
+  },
+  {
+    "revision": "0848d9d0f415ea63d3ecce98e586b461",
+    "url": "/static/media/counter-1-1600x600.0848d9d0.jpg"
+  },
+  {
+    "revision": "82c8fbe284a653442e684c21bbe4336c",
+    "url": "/static/media/logo-white.82c8fbe2.png"
+  },
+  {
+    "revision": "884a06cb027b4caf78dcba4959b58c34",
+    "url": "/static/media/mana2.884a06cb.jpg"
+  },
+  {
+    "revision": "dc439258993066c26ea3761ce1a3c3e5",
+    "url": "/static/media/manoj1.dc439258.png"
+  },
+  {
+    "revision": "0403d1502015855d1aaaab005caff9c7",
+    "url": "/static/media/met3.0403d150.jpg"
+  },
+  {
+    "revision": "99e3843bf880c87d204db13cd11f7ff4",
+    "url": "/static/media/met4.99e3843b.jpg"
+  },
+  {
+    "revision": "7c921ddc6f8690e43063bac6ea6d4341",
+    "url": "/static/media/met5.7c921ddc.jpg"
+  },
+  {
+    "revision": "bf69246006360e41647ecae601e247fa",
+    "url": "/static/media/slider-1-1600x900.bf692460.jpg"
+  },
+  {
+    "revision": "19ae42dbe0f8ac04f58993f4e70425a6",
+    "url": "/static/media/w22.19ae42db.png"
+  },
+  {
+    "revision": "1f0312b65ec66e7a3ade40aecf48a6af",
+    "url": "/static/media/webzed-font-1.1f0312b6.woff"
+  },
+  {
+    "revision": "6ed2107f2da89dcf9e52f006e878b679",
+    "url": "/static/media/webzed-font-1.6ed2107f.ttf"
+  },
+  {
+    "revision": "d5939ecfb23c9b4504264e0c4234ba30",
+    "url": "/static/media/webzed-font-1.d5939ecf.eot"
+  },
+  {
+    "revision": "e65681d7a99427774c50261520216889",
+    "url": "/static/media/webzed-font-1.e65681d7.svg"
+  }
+]);
